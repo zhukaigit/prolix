10c0aaf5441f9944f8255ce45a1fe83ba824ed5b
zhukai <zhukaixlyx@sina.com>
2019-02-14 13:17:03 +0800

    【mongo】GridFsTemplate测试实现
    (cherry picked from commit 54fd0be)

--- springboot-swagger2/src/test/java/com/zk/springbootswagger2/mongo/GridFsTemplateTest.java
+++ springboot-swagger2/src/test/java/com/zk/springbootswagger2/mongo/GridFsTemplateTest.java
@@ -2,14 +2,16 @@ package com.zk.springbootswagger2.mongo;
 
 import com.mongodb.client.gridfs.model.GridFSFile;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
@@ -25,33 +27,28 @@ public class GridFsTemplateTest {
 
   @Autowired
   private GridFsTemplate gridFsTemplate;
-  @Autowired
-  private ApplicationContext applicationContext;
 
   @Test
   public void testStoreFileInGridFs() throws Exception {
     File file = null;
     try {
-      file = ResourceUtils.getFile("classpath:application.properties");
+      file = ResourceUtils.getFile("classpath:mongo.properties");
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
-//    FileInputStream inputStream = new FileInputStream(file);
-//    ObjectId objectId = gridFsTemplate.store(inputStream, file.getName());
-//    System.out.println("存储的id为：" + objectId);
+    FileInputStream inputStream = new FileInputStream(file);
+    ObjectId objectId = gridFsTemplate.store(inputStream, file.getName());
+    System.out.println("存储的id为：" + objectId);
     System.out.println("文件名称："+file.getName());
   }
 
   @Test
   public void testFindFIleInGridFs() throws IOException {
-//    GridFSFile gfsFile = gridFsTemplate.findOne(Query.query(Criteria.where("_id").is("5c45a20111d83807f254eb2f")));
-    GridFSFile gfsFile = gridFsTemplate.findOne(Query.query(new Criteria()));
-
-    // 查询单个文件
-    GridFsResource gridFsResource = new GridFsResource(gfsFile);
-    InputStream inputStream = gridFsResource.getInputStream();
-
-    System.out.println("文件字符串如下：\n" + StreamUtils.copyToByteArray(inputStream).length);
+    GridFSFile gfsFile = gridFsTemplate
+        .findOne(Query.query(Criteria.where("filename").is("mongo.properties")));
+    GridFsResource resource = gridFsTemplate.getResource(gfsFile);
+    InputStream inputStream = resource.getInputStream();
+    System.out.println("文件字符串如下：\n" + StreamUtils.copyToString(inputStream, Charset.forName("utf-8")));
   }
 
 }
