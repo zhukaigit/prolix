770f06abd56375d5639a618cf7c58aea4b728807
zhukai <zhukaixlyx@sina.com>
2019-03-12 10:28:28 +0800

    spring binder测试

---
+++ springboot-test/src/main/java/com/zk/config/BinderTestModel.java
@@ -0,0 +1,9 @@
+package com.zk.config;
+
+import lombok.Data;
+
+@Data
+public class BinderTestModel {
+  private String name;
+  private String age;
+}
--- springboot-test/src/main/java/com/zk/config/MyConfig.java
+++ springboot-test/src/main/java/com/zk/config/MyConfig.java
@@ -1,14 +1,27 @@
 package com.zk.config;
 
 import com.zk.lifeCycle.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class MyConfig {
 
+  @Autowired
+  private Environment environment;
+
   @Bean(initMethod = "myInit", destroyMethod = "myDestroy")
   public Person person() {
     return new Person();
+  }
+
+  @Bean
+  public BinderTestModel binderTestModel() {
+    Binder binder = Binder.get(environment);
+    BinderTestModel model = binder.bind("test.binder", BinderTestModel.class).get();
+    return model;
   }
 }
