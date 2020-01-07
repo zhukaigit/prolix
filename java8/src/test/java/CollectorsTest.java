ab32cda5e57f8e2df8ecfcaa16fec62b24992386
zhukai001 <zhukai001@zhongan.com>
2020-01-07 10:14:31 +0800

    JDK8:Stream相关api操作测试2

--- java8/src/test/java/CollectorsTest.java
+++ java8/src/test/java/CollectorsTest.java
@@ -1,11 +1,148 @@
 import org.junit.Test;
 
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CollectorsTest {
+
+    // 求平均值
+    @Test
+    public void testAveraging () {
+        Double collect = Stream.of(1, 2, 3).collect(Collectors.averagingInt(e -> e));
+        System.out.println(collect);
+    }
+
+    // 流式处理之后再进行整合返回
+    @Test
+    public void testCollectingAndThen () {
+        // 返回：各个元素减5之后的平均值*100
+        Double collect = Stream.of(10, 5, 15).collect(
+                Collectors.collectingAndThen(
+                        Collectors.averagingDouble(e -> (double) (e - 5)), // 对各元素减5后求平均值
+                        e -> e * 100)   // 对上面的平均值*100
+        );
+        System.out.println(collect);
+    }
+
+    // 返回次数
+    @Test
+    public void testCounting () {
+        // 统计元素的个数
+        Long collect = Stream.of(10, 5, 15).collect(
+                Collectors.counting()
+        );
+        System.out.println(collect);
+    }
+
+    // 分组
+    @Test
+    public void testGroupingByConcurrent () {
+        ConcurrentMap<Integer, List<Integer>> collect = Stream.of(10, 5, 15, 10).collect(
+                Collectors.groupingByConcurrent(e -> e)
+        );
+        System.out.println(collect);
+
+        ConcurrentMap<Integer, Long> collect2 = Stream.of(10, 5, 15, 10).collect(
+                Collectors.groupingByConcurrent(
+                        e -> e,
+                        Collectors.counting()
+                )
+        );
+        System.out.println(collect2);
+    }
+
+    // 对各个元素单独处理之后，再进行流式处理
+    @Test
+    public void testMapping () {
+        // 返回：字符串转成数值后的平均值
+        Double collect = Stream.of("1", "2", "3").collect(
+                Collectors.mapping(
+                        Double::valueOf,//先对各个元素进行处理，返回元素T做为downStream的入参元素
+                        Collectors.averagingDouble(e -> (double) e)
+                )
+        );
+        // 简写如下
+//        Double collect = Stream.of("1", "2", "3").map(Double::valueOf).collect(Collectors.averagingDouble(e -> (double) e));
+        System.out.println(collect);
+    }
+
+    // 最大值，minBy不再演示
+    @Test
+    public void testMaxBy () {
+        Optional<Integer> collect = Stream.of(1, 2, 3).collect(
+                Collectors.maxBy(Integer::compareTo)
+        );
+        // 简写如下
+//        Optional<Integer> collect = Stream.of(1, 2, 3).max(Integer::compareTo);
+        System.out.println(collect);
+    }
+
+    // 分区
+    @Test
+    public void testPartitioningBy () {
+        // 按照奇偶数分区
+        Map<Boolean, List<Integer>> collect = Stream.of(1, 2, 3, 4, 5, 6, 7, 8).collect(
+                Collectors.partitioningBy(
+                        (Integer e) -> e % 2 == 0,
+                        Collectors.toList()
+                )
+        );
+        System.out.println(collect);
+    }
+
+    // reducing按照自己想要的规则，累计流中各个元素
+    @Test
+    public void testReducing () {
+        Optional<Integer> collect = Stream.of(1, 2, 3).collect(
+                Collectors.reducing((Integer e1, Integer e2) -> e1 + e2)
+        );
+        // 简写如下
+//        Optional<Integer> collect = Stream.of(1, 2, 3).reduce((Integer e1, Integer e2) -> e1 + e2);
+        System.out.println(collect);
+    }
+
+    // 收集
+    @Test
+    public void testReducing2 () {
+        Integer collect = Stream.of(1, 3, 4).collect(
+                Collectors.reducing(
+                        1000,// reducing方法的第三个参数BinaryOperator.apply(t,u)第一次执行时传入的初始化值
+                        x -> x + 1,// 对流中的各个元素进行映射处理
+                        (sum, b) -> {// 轮询归纳收集操作，sum即上次轮询返回结果，若第一次轮询，sum值即是第一个参数identity的值
+                            int nextSum = sum + b;
+                            System.out.printf("sum = %s, 当前轮询元素 = %s, nextSum = %s\n", sum, b, nextSum);
+                            //返回值作为下一次循环的sum值
+                            return nextSum;
+                        })
+        );
+
+        // 优雅简化
+//        Integer collect = Stream.of(1, 3, 4).map(x -> x + 1)
+//                .reduce(1000, (sum, b) -> {// 轮询归纳收集操作，sum即上次轮询返回结果，若第一次轮询，sum值即是第一个参数identity的值
+//                    int nextSum = sum + b;
+//                    System.out.printf("sum = %s, 当前轮询元素 = %s, nextSum = %s\n", sum, b, nextSum);
+//                    //返回值作为下一次循环的sum值
+//                    return nextSum;
+//                });
+        System.out.println(collect);
+    }
+
+    // 收集成结合
+    @Test
+    public void testToCollection () {
+        HashSet<Integer> collect = Stream.of(1, 3, 4, 1).collect(
+                Collectors.toCollection(HashSet::new)
+        );
+        System.out.println(collect);
+
+
+        Stream.of(1, 3, 4, 1).collect(
+                Collectors.toCollection(ArrayList::new)
+        );
+    }
 
 
     @Test
